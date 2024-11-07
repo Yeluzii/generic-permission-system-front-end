1af22163890df09c614ce6c934c6771ec810c37b
Yeluzii <2317413623@qq.com>
2024-11-07 18:49:22 +0800

    feat: 安装 pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,37 @@
 <template>
-  <div>
-    <h1>Hello, Element Plus!</h1>
-    <el-button type="primary" @click="handleClick"> 点击我 </el-button>
-    <div style="margin-top: 20px; font-size: 32px">
-      <el-icon>
-        <Plus />
-      </el-icon>
-    </div>
-  </div>
+  <h1>{{ store.count }}</h1>
+  <h2>{{ count }}</h2>
+  <h2>{{ msg }}</h2>
+  <el-button type="primary" size="default" @click="addBtn">新增</el-button>
 </template>
+
 <script setup lang="ts">
-import { ElButton, ElIcon } from 'element-plus'
-import { Plus } from '@element-plus/icons-vue'
-// 注册组件
-const handleClick = () => {
-  // console.log('按钮被点击')
+import { useTestStore } from '@/store/test'
+import { storeToRefs } from 'pinia'
+
+// 使用 storeToRefs 批量结构 Store 中的响应式数据
+const store = useTestStore()
+const { count, msg } = storeToRefs(store)
+
+const addBtn = () => {
+  // 第一种
+  // store.count++;
+
+  // 第二种
+  // store.setCount(++store.count)
+
+  // 第三种
+  // store.$patch({
+  //   count: ++store.count
+  // })
+
+  // 第四种
+  store.$patch((state) => {
+    state.count = ++state.count
+  })
 }
 </script>
+
 <style scoped>
 h1 {
   color: #42b983;
