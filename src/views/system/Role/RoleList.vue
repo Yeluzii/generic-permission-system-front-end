5f9ae5cb9c7aa64356e50ef6057560d95c700a0b
Yeluzii <2317413623@qq.com>
2024-11-11 15:52:11 +0800

    feat: 分配菜单页面制作

--- src/views/system/Role/RoleList.vue
+++ src/views/system/Role/RoleList.vue
@@ -4,7 +4,7 @@
     <el-form :model="searchParm" :inline="true" size="default">
       <el-form-item>
         <el-input
-          placeholder="请输⼊关键字"
+          placeholder="请输入关键字"
           v-model="searchParm.roleName"
         ></el-input>
       </el-form-item>
@@ -18,9 +18,9 @@
     </el-form>
     <!-- 表格数据 -->
     <el-table :height="tableHeight" :data="tableList" border stripe>
-      <el-table-column prop="roleName" label="⻆⾊名称"></el-table-column>
-      <el-table-column prop="remark" label="⻆⾊备注"></el-table-column>
-      <el-table-column label="操作" width="220" align="center">
+      <el-table-column prop="roleName" label="角色名称"></el-table-column>
+      <el-table-column prop="remark" label="角色备注"></el-table-column>
+      <el-table-column label="操作" width="320" align="center">
         <template #default="scope">
           <el-button
             type="primary"
@@ -28,6 +28,13 @@
             size="default"
             @click="editBtn(scope.row)"
             >编辑</el-button
+          >
+          <el-button
+            type="success"
+            icon="Edit"
+            size="default"
+            @click="assignBtn(scope.row)"
+            >分配菜单</el-button
           >
           <el-button
             type="danger"
@@ -39,7 +46,7 @@
         </template>
       </el-table-column>
     </el-table>
-    <!-- 分⻚ -->
+    <!-- 分页 -->
     <el-pagination
       @size-change="sizeChange"
       @current-change="currentChange"
@@ -51,6 +58,7 @@
       background
     >
     </el-pagination>
+
     <!-- 新增、编辑弹框 -->
     <SysDialog
       :title="dialog.title"
@@ -69,7 +77,7 @@
           :inline="false"
           size="default"
         >
-          <el-form-item prop="roleName" label="⻆⾊名称">
+          <el-form-item prop="roleName" label="角色名称">
             <el-input v-model="addModel.roleName"></el-input>
           </el-form-item>
           <el-form-item prop="remark" label="备注">
@@ -78,20 +86,26 @@
         </el-form>
       </template>
     </SysDialog>
+
+    <!-- 分配菜单 -->
+    <AssignTree ref="assignTree"></AssignTree>
   </el-main>
 </template>
+
 <script setup lang="ts">
 import { nextTick, onMounted, reactive, ref } from 'vue'
 import SysDialog from '@/components/SysDialog.vue'
 import useDialog from '@/hooks/useDialog'
 import { ElMessage, FormInstance } from 'element-plus'
-import { addApi, getListApi, editApi, deleteApi } from '@/api/role'
+import { addApi, getListApi, editApi, deleteApi } from '@/api/role/index'
 import { SysRole } from '@/api/role/RoleModel'
-
 import useInstance from '@/hooks/useInstance'
+import AssignTree from './AssignTree.vue'
+
+//菜单树的ref属性
+const assignTree = ref()
 //获取全局golbal
 const { global } = useInstance()
-
 //表单ref属性
 const addRef = ref<FormInstance>()
 //弹框属性
@@ -104,20 +118,18 @@ const searchParm = reactive({
   total: 0
 })
 
-// 判断新增还是编辑的表示 0：新增 1：编辑
+//判断新增还是编辑的标识 0:新增 1：编辑
 const tags = ref('')
-//新增按钮点击事件
+//新增按钮
 const addBtn = () => {
   tags.value = '0'
   dialog.title = '新增'
   dialog.height = 180
-
   //显示弹框
   onShow()
   //清空表单
   addRef.value?.resetFields()
 }
-
 //新增表单对象
 const addModel = reactive({
   roleId: '',
@@ -129,54 +141,30 @@ const rules = reactive({
   roleName: [
     {
       required: true,
-      message: '请输⼊⻆⾊名称',
+      message: '请输入角色名称',
       trigger: 'change'
     }
   ]
 })
-
-//表单提交
-const commit = () => {
-  addRef.value?.validate(async (valid) => {
-    if (valid) {
-      console.log('表单验证通过')
-      // 提交请求
-      let res = null
-      if (tags.value == '0') {
-        // 新增
-        res = await addApi(addModel)
-      } else {
-        res = await editApi(addModel)
-      }
-
-      if (res && res.code == 200) {
-        ElMessage.success(res.msg)
-        // 刷新数据
-        getList()
-        //关闭弹框
-        onClose()
-      }
-    }
-  })
-}
-
 //编辑按钮
 const editBtn = (row: SysRole) => {
   tags.value = '1'
-  // 打印传入的行数据
   console.log(row)
-  // 显示弹框
+  //显示弹框
   dialog.visible = true
   dialog.title = '编辑'
   dialog.height = 180
   nextTick(() => {
-    // 回显数据
+    //回显数据
     Object.assign(addModel, row)
   })
-  // 清空表单
+  //清空表单
   addRef.value?.resetFields()
 }
-
+//分配菜单按钮
+const assignBtn = (row: SysRole) => {
+  assignTree.value.show(row.roleId, row.roleName)
+}
 //删除按钮
 const deleteBtn = async (roleId: string) => {
   console.log(roleId)
@@ -192,48 +180,64 @@ const deleteBtn = async (roleId: string) => {
     }
   }
 }
-
-// 页容量改变时触发
+//页容量改变时触发
 const sizeChange = (size: number) => {
   searchParm.pageSize = size
   getList()
 }
-
-// 页数改变时触发
+//页数改变时触发
 const currentChange = (page: number) => {
   searchParm.currentPage = page
   getList()
 }
-
-// 表格高度
+//表单提交
+const commit = () => {
+  addRef.value?.validate(async (valid) => {
+    if (valid) {
+      console.log('表单验证通过')
+      //提交请求
+      let res = null
+      if (tags.value == '0') {
+        //新增
+        res = await addApi(addModel)
+      } else {
+        res = await editApi(addModel) //编辑
+      }
+      if (res && res.code == 200) {
+        ElMessage.success(res.msg)
+        //刷新列表
+        getList()
+        //关闭弹框
+        onClose()
+      }
+    }
+  })
+}
+//表格高度
 const tableHeight = ref(0)
 //表格数据
 const tableList = ref([])
-
 //查询列表
 const getList = async () => {
   let res = await getListApi(searchParm)
   if (res && res.code == 200) {
-    // 设置表格数据
+    //设置表格数据
     console.log(res)
     tableList.value = res.data.records
     //设置分页总条数
     searchParm.total = res.data.total
   }
 }
-
 //搜索
 const searchBtn = () => {
   getList()
 }
-
 //重置
 const resetBtn = () => {
   searchParm.roleName = ''
   searchParm.currentPage = 1
   getList()
 }
-
 onMounted(() => {
   nextTick(() => {
     tableHeight.value = window.innerHeight - 230
@@ -241,3 +245,5 @@ onMounted(() => {
   getList()
 })
 </script>
+
+<style scoped></style>
