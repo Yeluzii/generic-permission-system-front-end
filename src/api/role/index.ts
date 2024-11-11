cb83e6d64edcb0c293f3f43fad0fae5eb8b5ba9a
Yeluzii <2317413623@qq.com>
2024-11-11 16:11:19 +0800

    feat: 分配权限树保存与对接

--- src/api/role/index.ts
+++ src/api/role/index.ts
@@ -1,5 +1,5 @@
 import http from '@/http'
-import { SysRole, RoleListParm } from './RoleModel'
+import { SysRole, RoleListParm, SaveMenuParm } from './RoleModel'
 //新增
 export const addApi = (parm: SysRole) => {
   return http.post('/api/role', parm)
@@ -23,4 +23,9 @@ export const deleteApi = (roleId: string) => {
 // 角色下拉
 export const getSelectApi = () => {
   return http.get('/api/role/selectList')
+}
+
+//分配菜单保存
+export const saveRoleMenuApi = (parm: SaveMenuParm) => {
+  return http.post('/api/role/saveRoleMenu', parm)
 }
